7ac1e482b94c2925787ff07a76803921da12208c
Tataata13 <emili.edel.ten@gmail.com>
2024-05-31 21:05:16 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [towers, setTowers] = useState(data);
+    
+    const removeTower =(id)=> {
+      let newTowers = towers.filter(tower => tower.id !== id);
+      setTowers(newTowers)
+    }
+
+  return (<div className='container'>
+    <div>
+      <h1> the {towers.length} of tallest buildings in the world</h1>
     </div>
+    {towers.map((element => {
+      const{id, name, options, location, description, image} = element;
+
+      return ( <div>
+        <div className ='head' key={id}>
+        <div className ='top'>
+          <h2>{id} - {name}</h2>
+        </div>
+        <div className ='top'> 
+          <img src={image} alt="foto" width="250px"/>
+        </div>
+        <div className ='top'> 
+          <h4>{options}</h4>
+        </div>
+        <div className ='top'> 
+          <h3>{location}</h3>
+        </div>
+        <div> 
+          <h4>{description}</h4>
+        </div>
+        <div>
+          <button onClick={()=> {removeTower(id)}}>Remove</button>
+        </div>
+      </div>
+      </div>
+    );
+    }))}
+    <div className='top'>
+    <button  onClick={() => setTowers([])}>delete all</button>
+    </div>
+  </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,51 @@
+
+export const data = [
+{
+    id: 1,
+    name: "The Burj Khalifa",
+    options: "height: 828 m, floors: 163 (+ 2 below ground)",
+    location: "Dubai, United Arab Emirates",
+    description: "The Burj Khalifa is the world's tallest structure. With a total height of 829.8 m and a roof height excluding antenna, but including a 242.6 m spire of 828 m, the Burj Khalifa has been the tallest structure and building in the world since its topping out in 2009, surpassing Taipei 101, the previous holder of that status.",
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Burj_Khalifa.jpg/800px-Burj_Khalifa.jpg"
+},
+{
+    id: 2,
+    name: "Merdeka 118",
+    options: "height: 678.9 m, floors: 118 (+ 5 below ground)",
+    location: "city: Kuala Lumpur, country: Malaysia",
+    description: "The building's name, Merdeka, which means independence in Malay, is inspired by its proximity to Stadium Merdeka.The spire of the building was completed in December 2022, which marked its final height of 678.9 m (2,227 ft) above ground and 700.9 m (2,300 ft) above sea level.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Merdeka_118_20230317.jpg/800px-Merdeka_118_20230317.jpg"
+},
+{
+    id:3,
+    name: "Shanghai Tower",
+    options: "height: 632 m, floors: 128(+5 below ground)",
+    location: "city: Shanghai, country: China",
+    description: "Shanghai Tower is the tallest and largest LEED Platinum certified building in the world since 2015. Designed by the international design firm Gensler and owned by the Shanghai Municipal Government, it is the tallest of the world's first triple-adjacent supertall buildings in Pudong. Its tiered construction, designed for high energy efficiency, provides nine separate zones divided between office, retail and leisure use.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/e/ed/Shanghai_Tower_in_2015_%282%29.jpg"
+},
+{
+    id:4,
+    name: "Abraj Al-Bait Clock Tower",
+    options: "height: 601 m, floors: 120(+3 below ground)",
+    location: "city: Mecca, country: Saudi Arabia",
+    description: "These towers are a part of the King Abdulaziz Endowment Project that aims to modernize the city in catering to its pilgrims. The central hotel tower, which is the Makkah Clock Royal Tower, is the fourth-tallest building and sixth-tallest freestanding structure in the world. The clock tower contains the Clock Tower Museum that occupies the top four floors of the tower.",
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Abraj-al-Bait-Towers.JPG/800px-Abraj-al-Bait-Towers.JPG"
+},
+{
+id: 5,
+name: "Ping An International Finance Centre",
+options: "height: 599.1 m, floors: 115(+5 below ground)",
+location: "city: Shenzhen, country: China",
+description: "The building is located within the Central Business District of Shenzhen in Futian. Its 18,931 m2 lot was purchased by Ping An Group via auction at a price of 1.6568 billion RMB on 6 November 2007. Design of the building began in 2008 with Kohn Pedersen Fox Associates providing the architectural design and Thornton Tomasetti providing structural design. Its foundation stone was laid on 29 August 2009, and construction started in November the same year. China Construction First Building Group was hired as the general contractor to construct the building.",
+image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Pingan_International_Finance_Center2020.jpg/800px-Pingan_International_Finance_Center2020.jpg"
+},
+{
+id:6,
+name: "Lotte World Tower",
+options: "height: 554.5 m, floors: 123 (+6 below ground)",
+location: "city: Seoul, country: South Korea",
+description: "Lotte World Tower supertall skyscraper, located in Sincheon-dong, Songpa-gu, Seoul, South Korea. It is the sixth-tallest building in the world, the tallest in OECD countries, and also the first in South Korea to be over 100 stories tall.The tower opened to the public on April 3. A Sky Bridge Tour is located on the roof of Lotte World Tower at 541 m  which is the total height of One World Trade Center in New York City, the tallest building in the United States.",
+image: "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Lotte_World_Tower_2019.jpg/800px-Lotte_World_Tower_2019.jpg"
+}
+]
